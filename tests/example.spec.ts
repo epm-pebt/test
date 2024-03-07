b3c7c321e83fd18f1b2937b36053cd041279d0f9
VitalikTR <vitaliktr@gmail.com>
2024-03-07 12:13:45 +0200

    fixed endOfLine: crlf

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,20 +3,15 @@ import { allure } from 'allure-playwright';
 
 test('has title', async ({ page }) => {
     await page.goto('https://playwright.dev/');
-
     // Expect a title "to contain" a substring.
-
     await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', { tag: '@smoke' }, async ({ page }) => {
     await allure.owner('John Doe');
-
     page.goto('https://playwright.dev/');
-
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
-
     // Expects page to have a heading with the name of Installation.
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
